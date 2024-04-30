0bc306ab217ebd1dea5afb434593b7e44e4cf29e
hjhj43 <hjhj43@DESKTOP-6H8C935>
2024-04-30 10:05:53 +0900

    유효성 검사 메시지 추가

--- SecretProject/src/main/java/secretproject/user/vo/UserVO.java
+++ SecretProject/src/main/java/secretproject/user/vo/UserVO.java
@@ -12,24 +12,24 @@ import secretproject.cmmn.vo.DefaultVO;
 @SuppressWarnings("serial")
 public class UserVO extends DefaultVO{
 	
-	@NotEmpty
-	@Pattern(regexp = "^[a-z0-9]{5,20}+$")
+	@NotEmpty(message = "아이디는 공백이 될 수 없습니다.")
+	@Pattern(regexp = "^[a-z0-9]{5,20}+$", message = "아이디는 5~20자 영문 소문자와 숫자만 입력하세요.")
 	private String userId; // 유저 아이디
 	
-	@NotEmpty
-	@Pattern(regexp = "^[a-z0-9]{5,20}+$")
+	@NotEmpty(message = "비밀번호는 공백이 될 수 없습니다.")
+	@Pattern(regexp = "^[a-z0-9]{5,20}+$", message = "비밀번호는 5~20자 영문 소문자와 숫자만 입력하세요.")
 	private String userPw; // 유저 비밀번호
 
-	@NotEmpty
-	@Pattern(regexp = "^[가-힣a-zA-Z]{2,15}$")
+	@NotEmpty(message = "이름은 공백이 될 수 없습니다.")
+	@Pattern(regexp = "^[가-힣a-zA-Z]{2,15}$", message = "이름은 2~15자 한글과 영어만 입력하세요.")
 	private String userName; // 유저 이름
 	
-	@NotEmpty
-	@Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+	@NotEmpty(message = "이메일은 공백이 될 수 없습니다.")
+	@Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "잘못된 이메일 형식입니다.")
 	private String userEmail; // 유저 이메일
 	
-	@NotEmpty
-	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
+	@NotEmpty(message = "전화번호는 공백이 될 수 없습니다.")
+	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "잘못된 전화번호 형식입니다.")
 	private String userPhone; // 유저 핸드폰번호
 	
 	private int userAuthNum;
