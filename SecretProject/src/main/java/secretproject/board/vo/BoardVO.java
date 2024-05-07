580564a21bc75c59677566074d6a3809ed49ab9b
hjhj43 <hjhj43@DESKTOP-6H8C935>
2024-05-08 08:42:48 +0900

    유효성 검사 기능 추가

--- SecretProject/src/main/java/secretproject/board/vo/BoardVO.java
+++ SecretProject/src/main/java/secretproject/board/vo/BoardVO.java
@@ -3,6 +3,7 @@ package secretproject.board.vo;
 import java.sql.Date;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springmodules.validation.bean.conf.loader.annotation.handler.Max;
 
 import lombok.Data;
 import secretproject.cmmn.vo.DefaultVO;
@@ -17,15 +18,17 @@ public class BoardVO extends DefaultVO{
 	
 	private int boardSn; // 게시글 순번
 
-    @NotEmpty(message = "Board title cannot be empty.")
+    @NotEmpty(message = "게시글 제목은 공백이 될 수 없습니다.")
+    @Max(value=100, message = "제목은 100자 이내로 작성해주세요.")
 	private String boardTitle; // 게시글 제목
 
-	@NotEmpty
+	@NotEmpty(message = "게시글 제목은 공백이 될 수 없습니다.")
+    @Max(value=200, message = "내용은 200자 이내로 작성해주세요.")
 	private String boardContent; // 게시글 내용
 	
 	private Date boardDate; // 게시글 등록일 (YYYY-MM-DD)
 	
+	@NotEmpty(message = "유저 아이디는 공백이 될 수 없습니다.")
 	private String boardRegisterId; // 게시글 등록 유저 아이디
 	
-
 }
